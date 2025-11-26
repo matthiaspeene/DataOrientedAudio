1d53ad9b5afd2df688b585f93ccbee4f3faf7b0b
Matthias <38812879+matthiaspeene@users.noreply.github.com>
2025-11-26 23:24:41 +0100

    Rename and refactor VoicePositioningSystem to VoiceFollowSystem
    VoicePositioningSystem and its job were renamed to VoiceFollowSystem and VoiceFollowJob, respectively. The job logic was refactored to use a separate lookup for position offsets and to simplify the execution method. Associated .meta files were updated to reflect the new naming.

---
+++ Assets/Scripts/Audio/Voice/Runtime/Systems/VoiceFollowSystem.cs
@@ -0,0 +1,80 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using DataOrientedAudio.Common;
+using DataOrientedAudio.Voice.Runtime;
+
+namespace DataOrientedAudio.Voice.Runtime.Systems
+{
+    #region Follow System
+
+    [UpdateInGroup(typeof(AudioVoiceLifecycleGroup))]
+    [BurstCompile]
+    public partial struct VoiceFollowSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<VoiceFollowsEntity>();
+            state.RequireForUpdate<VoiceActive>();
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            var job = new VoiceFollowJob
+            {
+                TransformLookup = SystemAPI.GetComponentLookup<LocalTransform>(true),
+                OffsetLookup = SystemAPI.GetComponentLookup<VoicePositionOffset>(true)
+            };
+
+            job.ScheduleParallel();
+        }
+    }
+
+    #endregion
+
+    #region Follow Job
+
+    [BurstCompile]
+    [WithAll(typeof(VoiceActive))]
+    public partial struct VoiceFollowJob : IJobEntity
+    {
+        #region Lookups
+
+        [ReadOnly]
+        public ComponentLookup<LocalTransform> TransformLookup;
+
+        [ReadOnly]
+        public ComponentLookup<VoicePositionOffset> OffsetLookup;
+
+        #endregion
+
+        #region Execute
+
+        private void Execute(
+            Entity entity,
+            ref LocalTransform transform,
+            in VoiceFollowsEntity follow)
+        {
+            if (!TransformLookup.TryGetComponent(follow.Target, out var targetTransform))
+                return;
+
+            float3 position = targetTransform.Position;
+
+            if (OffsetLookup.HasComponent(entity))
+            {
+                position += OffsetLookup[entity].Value;
+            }
+
+            transform.Position = position;
+            transform.Rotation = targetTransform.Rotation;
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
